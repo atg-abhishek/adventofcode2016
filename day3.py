876137737e2950fcdc53c72f61e9cc9310f05a99
atg.abhishek <atg.abhishek@gmail.com>
2016-12-03 14:12:53 -0500

    day 3 part 2 solved

--- day3.py
+++ day3.py
@@ -12,10 +12,31 @@ def day3():
         sides = l.split(" ")
         sides = [s for s in sides if s is not ""]
         sides = [int(s) for s in sides]
+        
         if valid_triangle(sides):
             count += 1
+    return count
+
+def day3_part2():
+    global lst
+    grid = []
+    for l in lst:
+        sides = l.split(" ")
+        sides = [s for s in sides if s is not ""]
+        sides = [int(s) for s in sides]
+        grid.append(sides)
+    count = 0
+    flattened = [item for sublist in grid for item in sublist]
+    for i in range(0,len(flattened)-6,9):
+        for j in range(0,3):
+            sides = []
+            sides.append(flattened[i+j])
+            sides.append(flattened[i+j+3])
+            sides.append(flattened[i+j+6])
+            if valid_triangle(sides):
+                count += 1
     return count 
-        
+
 def valid_triangle(li):
 
     if li[0] + li[1] <= li[2]:
@@ -26,4 +47,5 @@ def valid_triangle(li):
         return False
     return True
 
-pprint(day3())
+pprint(day3_part2())
+# pprint(day3())
