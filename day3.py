69b3b1fc2f2c6b61772810cb677fd4391bc1f28f
atg.abhishek <atg.abhishek@gmail.com>
2016-12-04 21:06:59 -0500

    added comments for day 3

--- day3.py
+++ day3.py
@@ -10,6 +10,7 @@ def day3():
     count = 0
     for l in lst:
         sides = l.split(" ")
+        # removing the blank entries in the list 
         sides = [s for s in sides if s is not ""]
         sides = [int(s) for s in sides]
         
@@ -26,8 +27,11 @@ def day3_part2():
         sides = [int(s) for s in sides]
         grid.append(sides)
     count = 0
+    # flattening the grid of values in to a vector 
     flattened = [item for sublist in grid for item in sublist]
+    # going up to -6 because iteration works by walking along every row of input and then getting the 3 values below them to form the sides for the triangle. Meaning 9 values are processed at a time.
     for i in range(0,len(flattened)-6,9):
+        # the following loop is used to walk along one row and collect the elements below it 
         for j in range(0,3):
             sides = []
             sides.append(flattened[i+j])
@@ -38,7 +42,7 @@ def day3_part2():
     return count 
 
 def valid_triangle(li):
-
+    # checking the side sum rule for triangles
     if li[0] + li[1] <= li[2]:
         return False
     if li[1] + li[2] <= li[0]:
