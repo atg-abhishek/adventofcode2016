3a4c34d8c3d100e1f5c246389a7feffe808a9532
atg.abhishek <atg.abhishek@gmail.com>
2016-12-08 11:03:13 -0500

    solved part 2 day 7

--- Solutions/day7.py
+++ Solutions/day7.py
@@ -7,12 +7,13 @@ with open("../data/day7.txt") as infile:
     lst = data.splitlines()
 
 # lst = ["abba[mnop]qrst","aaaa[qwer]tyui","ioxxoj[asdfgh]zxcvbn","abcd[bdib]lkoij[asdas]xyyx"] #test input 
+# lst = ["aba[bab]xyz","xyx[xyx]xyx","aaa[kek]eke","zazbz[bzb]cdb"] #test input for part 2 
 
-def day7():
+def day7_part1():
     global lst
     count = 0
     for l in lst:
-        pprint(l)
+        # pprint(l)
         hypernet_fail = False
         hypernets = re.findall(r"\[[a-z]*\]", l)
         remaining = re.sub(r"\[[a-z]*\]", "-",l)
@@ -30,6 +31,32 @@ def day7():
         
     return count
 
+def day7_part2():
+    global lst
+    count = 0
+    for l in lst:
+        
+        hypernets = re.findall(r"\[[a-z]*\]", l)
+        remaining = re.sub(r"\[[a-z]*\]", "-",l)
+        remaining = remaining.split("-")
+        d = set()
+        for r in remaining:
+            check_aba(r,d)
+        # pprint(d)
+        found_in_hypernet = False
+        for seq in d:
+            for h in hypernets:
+                if seq in h:
+                    # pprint(l)
+                    found_in_hypernet = True
+                    count += 1
+                    break
+            if found_in_hypernet:
+                break
+    return count
+            
+
+
 def check_abba(s):
     for i in range(0,len(s)-3):
         if s[i] == s[i+1]:
@@ -38,5 +65,13 @@ def check_abba(s):
             return True
     return False
 
+def check_aba(s,d):
+    for i in range(0,len(s)-2):
+        if s[i]==s[i+1]:
+            continue
+        if s[i] == s[i+2]:
+            d.add(s[i+1]+s[i]+s[i+1])
 
-pprint(day7())
+        
+
+pprint(day7_part2())
