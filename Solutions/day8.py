3c272cff26d279573ce9b8aa7cf9d7936c9b9ee5
atg.abhishek <atg.abhishek@gmail.com>
2016-12-08 23:52:36 -0500

    added commets

--- Solutions/day8.py
+++ Solutions/day8.py
@@ -7,12 +7,14 @@ with open("../data/day8.txt") as infile:
 
 # lst = ["rect 3x2", "rotate column x=1 by 1", "rotate row y=0 by 4", "rotate column x=1 by 1"] # test input 
 
+# main grid used to carry out all the operations
 grid = []
 height = 6
 width = 50
 for i in range(0,height):
     temp = []
     for j in range(0,width):
+        # empty indicates there it is off
         temp.append(" ")
     grid.append(temp)
 
@@ -21,6 +23,7 @@ def day8():
     global grid, height, width
     for l in lst:
         # pprint(l)
+        # following disassemble all the instructions 
         instructions = l.split(" ")
         if instructions[0] == 'rect':
             nums = instructions[1].split('x')
@@ -36,6 +39,7 @@ def day8():
         for j in range(0,width):
             if grid[i][j] == "*":
                 count += 1
+    # following creates a graphical display at the end 
     for i in range(0,height):
         row = grid[i]
         pprint("".join(row))
@@ -48,6 +52,7 @@ def rotate_row(row_num, amount):
     row = grid[row_num]
     res = [" " for i in range(0, len(row))]
     for i in range(0,len(row)):
+        # following helps with the rotation 
         res[(i+amount)%len(row)] = row[i]
     grid[row_num] = res
 
@@ -56,11 +61,14 @@ def rotate_col(col_num, amount):
     amount = int(amount)
     global grid, height, width
     temp = [" " for i in range(0,height)]
+    # following assimilates all the elements from the column into a single list so it is easy to process
     for i in range(0,height):
         temp[i] = grid[i][col_num]
     res = [" " for i in range(0,height)]
+    # filling the shifted list 
     for i in range(0,height):
         res[(i+amount)%height] = temp[i]
+    # reassigning the elements in the grid after the shift 
     for i in range(0,height):
         grid[i][col_num] = res[i]
 
@@ -70,6 +78,7 @@ def rect(a,b):
     b = int(b)
     for row in range(0, a):
         for col in range(0, b):
+            # star indicates that the pixel is lit up 
             grid[row][col] = "*"
 
 pprint(day8())
