0869560dce338d988d37bf48dce6d3f44e969b42
atg.abhishek <atg.abhishek@gmail.com>
2016-12-15 00:16:07 -0500

    solved part 2 day 14

--- Solutions/day14.py
+++ Solutions/day14.py
@@ -1,20 +1,25 @@
 from pprint import pprint 
 from collections import defaultdict
 from hashlib import md5
-import re
+import re,json
 
 d = defaultdict(list)
 li = []
 res = []
-
+count = 0
 def day14(salt, key_stretching):
-    global d, li
+    global d, li, count 
     index = 0
-    count = 0
     # initializing the list with a 1001 hashes 
     while(index<=1000):
         plaintext = salt + str(index)
-        hashtext = md5(plaintext.encode()).hexdigest().lower()
+        hashtext = plaintext
+        if key_stretching:
+            for i in range(0,2017):
+                hashtext = md5(hashtext.encode()).hexdigest().lower()
+                # plaintext = hashtext
+        else:
+            hashtext = md5(plaintext.encode()).hexdigest().lower()
         li.append(hashtext)
         triples = getTriples(hashtext)
         d[hashtext] = triples
@@ -24,14 +29,23 @@ def day14(salt, key_stretching):
         if len(d[testhash]) != 0:
             if check_valid_key(d[testhash][0][1],index-1000):
                 count += 1
+                pprint(count)
 
         plaintext = salt+str(index)
-        hashtext = md5(plaintext.encode()).hexdigest().lower()
+        hashtext = plaintext
+        if key_stretching:
+            for i in range(0,2017):
+                hashtext = md5(hashtext.encode()).hexdigest().lower()
+                # plaintext = hashtext
+        else:
+            hashtext = md5(plaintext.encode()).hexdigest().lower()
         li.append(hashtext)
         triples = getTriples(hashtext)
         d[hashtext] = triples
         index += 1
     
+    with open('day14_res_2016.json','w') as outfile:
+        json.dump({"res" : res},outfile)
     return res[-1][1]
 
 def getTriples(s):
@@ -44,8 +58,9 @@ def check_valid_key(letter, idx):
         trips = d[li[i]]
         trips = [x[0] for x in trips]
         if letter*5 in trips:
-            res.append((li[idx-1],idx-1,li[i],i))
+            res.append((li[idx-1],idx-1,li[i],i,count))
             return True
     return False
 
-pprint(day14("ahsbgdzn", False)) # for part 1 
+# pprint(day14("ahsbgdzn", False)) # for part 1 
+pprint(day14("ahsbgdzn", True))
